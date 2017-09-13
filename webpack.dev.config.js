2bd9eea8a1f34353d5ba4edfde10539c1aa0d353
Chester How <chesterhow@gmail.com>
2017-09-13 16:55:47 +0800

    Set up React and friends

---
+++ webpack.dev.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: {
+    main: [
+      'react-hot-loader/patch',
+      // activate HMR for react
+
+      'webpack-dev-server/client?http://localhost:8000',
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+
+      './src/index.js'
+      // entry point of the app
+    ]
+  },
+
+  output: {
+    filename: '[name].js',
+    // Not using [chunkhash] in dev as it increases compilation time
+
+    path: path.resolve(__dirname, 'dist'),
+
+    publicPath: ''
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }, {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: 'style-loader'
+          // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader'
+          // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader'
+          // compiles Sass to CSS
+        }, {
+          loader: 'postcss-loader'
+        }]
+      }, {
+        test: /\.(jpg|png|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+            name: '[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new HtmlWebpackPlugin({ template: 'index.html' })
+    // generates 'index.html' and handles 'script' and 'link' tags
+  ],
+
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true,
+
+    historyApiFallback: true,
+    // respond to 404s with index.html
+
+    hot: true,
+    // enable HMR on the server
+
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+
+    port: 8000
+  }
+};
+
+module.exports = config;
---
+++ webpack.prod.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const pathsToClean = ['dist'];
+
+const config = {
+  entry: {
+    main: [
+      './src/index.js'
+    ],
+    // entry point of the app
+
+    vendor: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'prop-types'
+    ]
+    // global dependencies
+  },
+
+  output: {
+    filename: '[name].[chunkhash:8].js',
+
+    path: path.resolve(__dirname, 'dist'),
+
+    publicPath: ''
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }, {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader', 'postcss-loader']
+        })
+      }, {
+        test: /\.(jpg|png|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+            name: '[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    // split global dependencies into a separate 'vendor' file.
+    // 'manifest' file extracts webpack's runtime code from the 'vendor'
+    // file. this prevents the 'vendor' file's chunkhash from changing
+    // every build.
+
+    new webpack.HashedModuleIdsPlugin(),
+    // ensures the 'vendor' file's chunkhash stays the same when code
+    // is modified
+
+    new ExtractTextPlugin('styles.[contentHash:8].css'),
+    // split css into separate file
+    // note: do not use this for dev as it does not work with HMR
+
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+    // generates 'index.html' and handles 'script' and 'link' tags
+
+    new CleanWebpackPlugin(pathsToClean, { verbose: true })
+    // removes 'dist' folder before building
+  ]
+};
+
+module.exports = config;
