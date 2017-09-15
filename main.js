d75b91fe0a05f421f35def5c78ea255e605186b2
Chester How <chesterhow@gmail.com>
2017-09-15 17:36:29 +0800

    Set up electron

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+  // Initialize the window to specified dimensions
+  win = new BrowserWindow({ width: 400, height: 200 });
+
+  // Specify entry point
+  win.loadURL('http://localhost:8000');
+
+  // Show dev tools
+  win.webContents.openDevTools();
+
+  // Remove window once app is closed
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
