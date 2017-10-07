8e152b1e04185cfe5b27ef9357a84c4bd4ad98f8
Chester How <chesterhow@gmail.com>
2017-10-07 09:37:36 +0800

    Fix broken styling

--- src/renderer/components/listing/listing.js
+++ src/renderer/components/listing/listing.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './styles.scss';
+import './styles.scss';
 
 const calcValue = (amount) => {
   // do calculations
   return amount;
 };
 
-const Listing = ({ currency, amount }) => (
-  <div className={styles.listing}>
-    <p className={styles.currency}>{currency}</p>
-    <p className={styles.amount}>{amount}</p>
-    <p className={styles.value}>{calcValue(amount)}</p>
+const Listing = ({ currency, amount, currencyCode }) => (
+  <div className="listing">
+    <p className="currency">{currency}</p>
+    <p className="amount">{amount}&nbsp;{currencyCode}</p>
+    <p className="value">$&nbsp;{calcValue(amount)}</p>
   </div>
 );
 
 Listing.propTypes = {
   currency: PropTypes.string.isRequired,
-  amount: PropTypes.number.isRequired
+  amount: PropTypes.number.isRequired,
+  currencyCode: PropTypes.string.isRequired
 };
 
 export default Listing;
