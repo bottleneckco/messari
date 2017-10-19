d1a74b8d373bcc42fad35d2f0c5154561dc11096
Duncan Leo <duncanleo97@gmail.com>
2017-10-19 14:18:13 +0800

    use input element to set cryptocurrency amount

--- src/renderer/components/listing/listing.js
+++ src/renderer/components/listing/listing.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const calcValue = (amount) => {
-  // do calculations
-  return amount;
-};
+class Listing extends Component {
+  constructor(props) {
+    super(props);
 
-const Listing = ({ currency, amount, currencyCode }) => (
-  <div className="listing">
-    <p className="currency">{currency}</p>
-    <p className="amount">{amount}&nbsp;{currencyCode}</p>
-    <p className="value">$&nbsp;{calcValue(amount)}</p>
-  </div>
-);
+    this.state = {
+      amount: this.props.amount
+    };
+
+    this.onAmountChange = this.onAmountChange.bind(this);
+    this.calcValue = this.calcValue.bind(this);
+  }
+
+  onAmountChange(event) {
+    this.setState({
+      amount: event.target.value
+    });
+  }
+
+  calcValue() {
+    // do calculations
+    return this.state.amount;
+  }
+
+  render() {
+    const { currency, currencyCode } = this.props;
+    const { amount } = this.state;
+    return (
+      <div className="listing">
+        <p className="currency">{currency}</p>
+        <div className="amount">
+          <input
+            name="amount"
+            defaultValue={amount}
+            type="number"
+            step="0.01"
+            min="0"
+            onChange={this.onAmountChange}
+          />
+          <label htmlFor="amount">{currencyCode}</label>
+        </div>
+        <p className="value">$&nbsp;{this.calcValue()}</p>
+      </div>
+    );
+  }
+}
 
 Listing.propTypes = {
   currency: PropTypes.string.isRequired,
